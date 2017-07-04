bc31eaaaa28cd40980cebce3eccac7018d4e1510
evertok <594996819@qq.com>
2017-07-04 18:42:19 +0800

    udate

--- StockAnalyze/GetStockData.py
+++ StockAnalyze/GetStockData.py
@@ -1,5 +1,4 @@
 import tushare as ts
-import time
 import datetime
 import pandas as pd
 
@@ -8,19 +7,31 @@ def getdata():
     codes = ts.get_stock_basics()
     codelist = pd.DataFrame()
     print("股票代码" + " " * 5 + "股票名称" + " " * 10)
-    tmp = {"code": [], "name": []}
-    for i in range(len(codes)):
-        if codes.index[i].startswith("3") or codes.name[i].startswith(
-                "*ST") or codes.name[i].startswith("N"):
+    result = {"code": [], "name": []}
+    yearpre2 = datetime.datetime.now() - datetime.timedelta(days=712)
+    yearpre2 = int(yearpre2.strftime("%Y%m%d"))  # 上市时间要超过两年
+    '''
+    iloc速度巨慢，是iterrows的3倍
+    print(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"))
+    for code in codes.index:
+        if code.startswith("3") or codes.ix[code]["name"].startswith(
+                "*ST") or codes.ix[code]["name"].startswith("N") or codes.ix[code]["timeToMarket"] > yearpre2:
             continue
-        tmp["code"].append(codes.iloc[i].name)
-        tmp["name"].append(codes.iloc[i]["name"])
-
-    codelist = pd.DataFrame(tmp, index=tmp["code"])
+        tmp["code"].append(code)
+        tmp["name"].append(codes.ix[code]["name"])
+    print(datetime.datetime.now().strftime("%Y%m%d %H:%M:%S"))
+    '''
+    tmpCode = result["code"]
+    tmpName = result["name"]
+    for val in codes.iterrows():
+        if val[0].startswith("3") or val[1]["name"].startswith(
+                "*ST") or val[1]["name"].startswith("N") or val[1]["timeToMarket"] > yearpre2:
+            continue
+        tmpCode.append(val[0])
+        tmpName.append(val[1]["name"])
+ 
     # for i in range(0, len(codelist)):
     #    print(codelist.iloc[i]["code"] + " " * 7 + codelist.iloc[i]["name"])
-    currTime = time.localtime()
-    today = time.strftime("%Y-%m-%d", currTime)
     now = datetime.datetime.now()
     preday30 = now + datetime.timedelta(days=-200)
 
@@ -28,15 +39,25 @@ def getdata():
     data_w = None
     # 股票历史数据
     good_data = []
-    for codeStr in tmp["code"]:
+    count = 0
+    for codeStr in result["code"]:
+        count += 1
+        print('正在进行第' + str(count) + "个数据处理，代码为：" + codeStr)
         data_d = ts.get_hist_data(
             codeStr, start=preday30.strftime("%Y-%m-%d"), ktype='D')
-        data_d.dropna(inplace=True)
+        if (data_d is None or len(data_d) == 0):
+            continue
         if (handleData(data_d)):
             data_w = ts.get_hist_data(
                 codeStr, start=preday30.strftime("%Y-%m-%d"), ktype='W')
-            data_w.rename(columns={"ma5": "ma25", "ma10": "ma50", "ma20": "ma100"}, inplace=True)  # 修改列名
-            if(handleWdata(pd.concat([data_d, data_w], axis=1, join_axes=[data_d.index]))):
+            data_w.rename(
+                columns={"ma5": "ma25",
+                         "ma10": "ma50",
+                         "ma20": "ma100"},
+                inplace=True)  # 修改列名
+            if (handleWdata(
+                    pd.concat(
+                        [data_d, data_w], axis=1, join_axes=[data_d.index]))):
                 good_data.append(codeStr)
     # 成交量大于vol(手)的股票
     # df = ts.get_sina_dd(codeStr, date=today, vol=500)
@@ -57,7 +78,7 @@ def handleData(data):
     if (data.iloc[i].ma10 <= data.iloc[i].ma20):
         return False
     sorted = data.sort_values(by="high", ascending=False)  # 降序排列
- 
+
     max_high = sorted.iloc[i].high
     max_date = sorted.iloc[i].name
     sorted = data.sort_values(by="low")  # 升序排列
@@ -73,20 +94,20 @@ def handleData(data):
     sorted1 = sorted[sorted.iloc[i].name:max_date].sort_values(by="low")
     min_low1 = sorted1.iloc[i].low
     curr_price = sorted.iloc[i].close
-    if(curr_price > max_high * 0.8):
+    if (curr_price > max_high * 0.8):
         return False
-    if(min_low1 < sorted1.iloc[i].ma20):
+    if (min_low1 < sorted1.iloc[i].ma20):
         return False
     return True
 
 
 def handleWdata(data):
     # 需要上升趋势
-    if (data.iloc[1].ma25 <= data.iloc[1].ma50):
+    i = 0
+    if (data.iloc[i].ma25 <= data.iloc[i].ma50):
         return False
-    if (data.iloc[1].ma50 <= data.iloc[1].ma100):
+    if (data.iloc[i].ma50 <= data.iloc[i].ma100):
         return False
-    
     return True
 
 
